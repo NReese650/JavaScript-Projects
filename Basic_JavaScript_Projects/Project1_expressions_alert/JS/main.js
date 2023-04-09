5b507337f271c8851bd60e92c8555e2dabd12c75
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-04-09 16:40:31 -0700

    Work
    Progressing through javascript course

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,41 @@
+document.write ('Hello, World!')                //Using the document.write prompt with a string
+
+var A = "This is a string";                     //Using a variable to write a string
+
+document.write(A)
+
+var myIdea = "This variable is my own idea";    //Creating my own variable and string
+
+window.alert(myIdea)                            //Using the window alert function to have the string display as a popup window
+
+document.write("This is a quote, \"I am not a crook\"");    //Using the \ to display restricted elements like the ""
+
+document.write("Here is one sentence"           //Using the + symbol to concatenate strings
++ "Here is another");
+
+
+var B = "Concatenated" + "String"               //Using the + symbol to concatenate strings in a variable
+document.write(B);
+
+var Family = "The Lannisters", Dad = "Tywin", Daughter = "Cersei", Son = "Jamie", Embarrassment = "Tyrion"; //Creating multiple variables 
+document.write(Dad);
+
+
+document.write(58 + 94);                //Using an expression as the string and receiving the correct output
+
+
+function My_First_Function() {                              //Defining a function and naming it
+    var str = "This is the button text";                    //Defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+
+//Basic Javascript Projects Submission Assignment
+
+var Sent1 = "This is the beginning of the string", Sent2 ="and this is the end of the string";  //creating two variables according to project requirements
+
+window.alert("This is a popup window alert");   //Using the window alert method
+
+document.write(Sent1 +  Sent2);                 //Using the document write method with two concatenated variablles
+
+document.write(74 + 89);                        //Writing an expression in the string value
