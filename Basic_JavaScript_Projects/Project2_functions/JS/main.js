3f26df9236f5432e18d327a8c8c724687c7e8019
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-04-10 17:56:33 -0700

    Changes
    Making changes to the HTML and JS files. I seem to have not saved the code when I finished working on it

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,29 +1,15 @@
-//Establishing an addition function
-function addition_Function() {
-    var addition = 5 + 5;
-    document.getElementById("Math").innerHTML = "5 + 5 = " + addition;
+//Creating a function to show the family names
+function familyName() {
+    var Lan = "Lannisters"
+    var Star = "The Starks"
+    document.getElementById("Button_Text").innerHTML = Lan;
+    document.getElementById("Button_Text2").innerHTML = Star;
 }
 
-//Establishing a subtraction function
-function subtraction_Function() {
-    var Subtraction =  5 - 2;
-    document.getElementById("Math1").innerHTML = "5 - 2 = "  + Subtraction ;
-}
 
-//Establishing a multiplication function
-function multiplication() {
-    var multiplication = 6 * 8;
-    document.getElementById("Math2").innerHTML = "6 x 8 = " + multiplication;
-}
-
-//Establishing a division function
-function division() {
-    var division = 987/5;
-    document.getElementById("Math3").innerHTML = "987 / 5 = " + division;
-}
-
-//Establishing a function that contains multiple operators
-function mix() {
-    var mixture = (5 + 5) * 9 / (5 - 2) * 13;
-    document.getElementById("Math4").innerHTML = "\ (5 + 5) * 9 / (5 - 2) * 13 = " + mixture;
+//Creating a function that uses the shorthand += to concatenate two sentences
+function myFunction() {
+    var sentence = "I am learning"
+    sentence += " a lot from this course";
+    document.getElementById("concatenate").innerHTML = sentence;
 }
