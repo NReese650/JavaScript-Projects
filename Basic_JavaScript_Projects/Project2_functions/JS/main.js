52ad6dbf9582ab585fc8933c627885214fd67af9
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-04-10 17:33:54 -0700

    Revising
    Revising project 2 assignment

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,4 +1,29 @@
+//Establishing an addition function
 function addition_Function() {
     var addition = 5 + 5;
     document.getElementById("Math").innerHTML = "5 + 5 = " + addition;
+}
+
+//Establishing a subtraction function
+function subtraction_Function() {
+    var Subtraction =  5 - 2;
+    document.getElementById("Math1").innerHTML = "5 - 2 = "  + Subtraction ;
+}
+
+//Establishing a multiplication function
+function multiplication() {
+    var multiplication = 6 * 8;
+    document.getElementById("Math2").innerHTML = "6 x 8 = " + multiplication;
+}
+
+//Establishing a division function
+function division() {
+    var division = 987/5;
+    document.getElementById("Math3").innerHTML = "987 / 5 = " + division;
+}
+
+//Establishing a function that contains multiple operators
+function mix() {
+    var mixture = (5 + 5) * 9 / (5 - 2) * 13;
+    document.getElementById("Math4").innerHTML = "\ (5 + 5) * 9 / (5 - 2) * 13 = " + mixture;
 }
